f1d3ec1bac39c9ca554e2f24a6d453cc41e6c9c0
Ryan Wans <37909218+ryanrocket@users.noreply.github.com>
2020-01-22 09:47:23 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1 style="font-weight: 500!important">{{ msg }}</h1>
+    <h1 style="font-weight: 500!important">websaite undefr c ontsuyrvction</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
