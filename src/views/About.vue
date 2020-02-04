57499b78fd8a94832c921782bdb5f94f5b43cb7a
Ryan <ryan@ryanwans.com>
2020-02-04 16:08:37 -0500

    MAKEFILE

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="about">
-    <script>
-      +(function() {
-        window.location.href = "https://www.bonfire.com/brazil-is-not-real-official-merch/";
-      })();
-    </script>
   </div>
 </template>
+
+<script>
+export default {
+  
+}
+window.location.href="https://www.bonfire.com/brazil-is-not-real-official-merch-1/";
+</script>
